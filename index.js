9ae27ddadb1e48d3854e04bfc3098d246f6ed5d0
Ivan <ivan.lyukshin@webpp.ru>
2023-09-19 17:45:34 +0300

    small refactoring

--- index.js
+++ index.js
@@ -1,76 +1,50 @@
-type Id = string | number;
-
-interface Item<T> {
-    id: Id;
-    parent: Id;
-    type?: T;
-}
-
-interface MapValue<T> {
-    children: Id[];
-    item: Item<T>;
-}
-
-class TreeStore<T> {
-    private map: Map<Id, MapValue<T>>;
-
-    constructor(private items: Item<T>[]) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.items = exports.TreeStore = void 0;
+class TreeStore {
+    constructor(items) {
+        this.items = items;
         this.setMap();
     }
-
-    getAll(): Item<T>[] {
+    getAll() {
         return this.items;
     }
-
-    getItem(id: Id): Item<T> {
+    getItem(id) {
         const node = this.map.get(id);
-
         if (!node) {
             throw new Error(`Attempt to get a non-existent object with id ${id}`);
         }
-
         return node.item;
     }
-
-    getChildren(id: Id): Item<T>[] {
+    getChildren(id) {
         const node = this.map.get(id);
-
         return node.children.map((id) => this.getItem(id));
     }
-
-    getAllChildren(id: Id): Item<T>[] {
+    getAllChildren(id) {
         const allChildren = [];
-        const IdsStack: Id[] = [id];
-
+        const IdsStack = [id];
         while (IdsStack.length) {
             const currId = IdsStack.shift();
-
             allChildren.push(...this.getChildren(currId));
             IdsStack.push(...this.map.get(currId).children);
         }
-
         return allChildren;
     }
-
-    getAllParents(id: Id): Item<T>[] {
+    getAllParents(id) {
+        var _a;
         let allParents = [];
         let currId = id;
-        let parentId: Id;
-
-        while ((parentId = this.map.get(currId)?.item.parent) !== 'root') {
+        let parentId;
+        while ((parentId = (_a = this.map.get(currId)) === null || _a === void 0 ? void 0 : _a.item.parent) !== 'root') {
             allParents.push(this.getItem(parentId));
             currId = parentId;
         }
-
         return allParents;
     }
-
-    private setMap() {
+    setMap() {
         this.map = new Map();
-
         for (let i = 0; i < this.items.length; i++) {
             const item = this.items[i];
-
             if (!this.map.has(item.id)) {
                 this.map.set(item.id, {
                     children: [],
@@ -78,34 +52,26 @@ class TreeStore<T> {
                 });
             }
         }
-
         for (let i = 0; i < this.items.length; i++) {
             const { id, parent } = this.items[i];
-
             const parentNode = this.map.get(parent);
-
-            if (!parentNode) continue;
-
+            if (!parentNode)
+                continue;
             const { children } = parentNode;
-
-            this.map.set(parent, { ...parentNode, children: [...children, id] });
+            this.map.set(parent, Object.assign(Object.assign({}, parentNode), { children: [...children, id] }));
         }
     }
 }
-
+exports.TreeStore = TreeStore;
 const items = [
     { id: 1, parent: 'root' },
     { id: 2, parent: 1, type: 'test' },
     { id: 3, parent: 1, type: 'test' },
-
     { id: 4, parent: 2, type: 'test' },
     { id: 5, parent: 2, type: 'test' },
     { id: 6, parent: 2, type: 'test' },
-
     { id: 7, parent: 4, type: null },
     { id: 8, parent: 4, type: null },
 ];
-
-const ts = new TreeStore<any>(items);
-
-export { TreeStore, items };
+exports.items = items;
+const ts = new TreeStore(items);
--- test.ts
+++ test.ts
@@ -1,18 +1,5 @@
 import { describe, expect, it } from '@jest/globals';
-import { TreeStore } from '.';
-
-const items = [
-    { id: 1, parent: 'root' },
-    { id: 2, parent: 1, type: 'test' },
-    { id: 3, parent: 1, type: 'test' },
-
-    { id: 4, parent: 2, type: 'test' },
-    { id: 5, parent: 2, type: 'test' },
-    { id: 6, parent: 2, type: 'test' },
-
-    { id: 7, parent: 4, type: null },
-    { id: 8, parent: 4, type: null },
-];
+import { TreeStore, items } from '.';
 
 function createTestTs() {
     return new TreeStore(items);
